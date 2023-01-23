fa663e5219fbffbc4a43b2ffe2d52f24539b5ed6
Ulises Maghenzani <ulicmgh@gmail.com>
2023-01-23 03:43:41 -0300

    first commit -setup

---
+++ apps/auth/src/auth.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get } from '@nestjs/common';
+import { RmqContext } from '@nestjs/microservices/ctx-host';
+import { Ctx, MessagePattern } from '@nestjs/microservices/decorators';
+import { AuthService } from './auth.service';
+
+@Controller()
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Get()
+  getHello(): string {
+    return this.authService.getHello();
+  }
+
+  @MessagePattern({ cmd: 'get-user' })
+  async getUser(@Ctx() context: RmqContext) {
+    const channel = context.getChannelRef();
+    const message = context.getMessage();
+    channel.ack(message);
+
+    return { user: 'USER' };
+  }
+}
